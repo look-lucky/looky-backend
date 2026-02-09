014b176a3823be179c298d46f3d92ca393d9ff5f
dohyeon <ehgus8910@gmail.com>
2026-02-09 14:59:55 +0900

    feat: 서버 시간 설정

--- src/main/java/com/looky/LookyApplication.java
+++ src/main/java/com/looky/LookyApplication.java
@@ -1,12 +1,21 @@
 package com.looky;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class LookyApplication {
+
+    @PostConstruct
+    public void started() {
+        // 기본 타임존을 서울로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LookyApplication.class, args);

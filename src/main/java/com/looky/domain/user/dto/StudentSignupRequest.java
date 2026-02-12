ba3a1157a55e1fc3b91b84bba17d5944e68a5033
dohyeon <ehgus8910@gmail.com>
2026-02-12 21:39:35 +0900

    fix: 요청 권한 문제 해결

--- src/main/java/com/looky/domain/user/dto/StudentSignupRequest.java
+++ src/main/java/com/looky/domain/user/dto/StudentSignupRequest.java
@@ -16,6 +16,7 @@ import java.time.LocalDate;
 public class StudentSignupRequest {
     @Pattern(regexp = "^[a-zA-Z0-9]{6,16}$", message = "아이디는 영문, 숫자 포함 6자 이상 16자 이하여야 합니다.")
     private String username; // 아이디 (이메일X)
+    @jakarta.validation.constraints.Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,20}$", message = "비밀번호는 영어, 숫자, 특수문자를 포함한 8자 이상 20자 이하여야 합니다.")
     private String password;
     private String email; 
     private String nickname;

65eef485ba6e10da15a4017cc3967f3bffe52cd2
dohyeon <ehgus8910@gmail.com>
2026-03-25 17:17:33 +0900

    chore: 미사용 예외/DTO/enum/repository 정리

--- src/main/java/com/looky/domain/event/repository/EventImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.looky.domain.event.repository;
-
-import com.looky.domain.event.entity.EventImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventImageRepository extends JpaRepository<EventImage, Long> {
-}
--- src/main/java/com/looky/domain/partnership/entity/PartnershipStatus.java
+++
@@ -1,8 +0,0 @@
-package com.looky.domain.partnership.entity;
-
-public enum PartnershipStatus {
-
-    ACTIVE, // 제휴 진행 중
-    EXPIRED, // 만료됨
-    SUSPENDED // 일시 정지
-}
